3a8400051c94eb5022c1fa8f6e2ea7c046845096
Mohammed Ashfaque <mohammedashfaquemvkpadi@gmail.com>
2025-08-23 03:47:07 +0530

    w4 started

---
+++ Week4/Day1/2.py
@@ -0,0 +1,5 @@
+🔹 What is SQLite3?
+SQLite is a lightweight database engine.
+Unlike MySQL or PostgreSQL, it doesn’t need a server.
+Data is stored in a single .db file → persistent storage.
+Good for small/medium apps, prototyping, desktop apps, or mobile apps.
---
+++ Week4/Day1/extra.py
@@ -0,0 +1,3 @@
+#     user_email = models.ForeignKey(Users, to_field='email', on_delete=models.CASCADE)  # ForeignKey to email field in Users model
+
+# by adding custom forign key
