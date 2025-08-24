d9eee5ce24cade5c84448f3dcd6417eae42c67b9
Mohammed Ashfaque <mohammedashfaquemvkpadi@gmail.com>
2025-08-24 22:29:33 +0530

    w4 s1

--- Week4/Day1/extra.py
+++ Week4/Day1/extra.py
@@ -1,3 +1,4 @@
 #     user_email = models.ForeignKey(Users, to_field='email', on_delete=models.CASCADE)  # ForeignKey to email field in Users model
 
-# by adding custom forign key
+# by adding custom forign key 
+// testing fot git add
