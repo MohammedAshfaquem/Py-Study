680ffdc9b1f02e5355456a28c3ad95ffe7b999cd
Mohammed Ashfaque <mohammedashfaquemvkpadi@gmail.com>
2025-09-28 20:14:04 +0530

    removed

--- Week5/Day3/2.py
+++ Week5/Day3/2.py
@@ -21,4 +21,3 @@
 # Meta = model configuration hub.
 # It customizes DB behavior, admin display, constraints, ordering, etc.
 # It’s optional → if you don’t define it, Django uses defaults.
-# added
